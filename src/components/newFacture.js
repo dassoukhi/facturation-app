9f501282401913d58d2145c5a62e1312eb3aad0e
Saleh DASSOUKHI <57576759+dassoukhi@users.noreply.github.com>
2021-08-26 23:14:27 +0200

    mj

--- src/components/newFacture.js
+++ src/components/newFacture.js
@@ -1,14 +1,31 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
-import { Button, makeStyles, Paper } from '@material-ui/core'
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  Paper,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider
 } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
 import TableFacture from './tableFacture'
+import styled from 'styled-components'
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+`
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -57,87 +74,122 @@ export default function NewFacture({ handleStatus }) {
   }
 
   return (
-    <main className={classes.layout}>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Paper className={classes.paper}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Button onClick={handleStatus} color='secondary'>
-                Annuler
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-              <TextField
-                required
-                id='client'
-                name='client'
-                label='Client'
-                fullWidth
-                size='small'
-                variant='outlined'
-                autoComplete='given-name'
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                required
-                id='numFacture'
-                name='numFacture'
-                label='Numéro de Facture'
-                variant='outlined'
-                size='small'
-                fullWidth
-                autoComplete='family-name'
-              />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <TextField
-                required
-                id='devise'
-                name='devise'
-                label='Devise'
-                variant='outlined'
-                size='small'
-                fullWidth
-                autoComplete='family-name'
-              />
-            </Grid>
-            <Grid
-              container
-              spacing={0}
-              direction='row'
-              justifyContent='center'
-              color='#252831'
+    <Fragment>
+      <Nav>
+        <AppBar position='absolute' color='white' className={classes.appBar}>
+          <Toolbar>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'space-between'
+              }}
             >
-              <Grid item xs={1} sm={4}>
-                <KeyboardDatePicker
-                  disableToolbar
+              <div>
+                <Typography variant='h6' color='inherit' noWrap>
+                  <Button
+                    variant='contained'
+                    onClick={handleStatus}
+                    color='secondary'
+                  >
+                    Annuler
+                  </Button>
+                </Typography>
+              </div>
+              <div>
+                <Typography variant='h6' color='inherit' noWrap>
+                  <Button
+                    variant='contained'
+                    onClick={handleStatus}
+                    color='primary'
+                  >
+                    Enregistrer
+                  </Button>
+                </Typography>
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </Nav>
+      <main className={classes.layout}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Paper className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12} sm={5}>
+                <TextField
+                  required
+                  id='client'
+                  name='client'
+                  label='Client'
+                  fullWidth
+                  size='small'
                   variant='outlined'
-                  format='MM/dd/yyyy'
-                  margin='normal'
-                  id='date-creation'
-                  label='Date de la création'
-                  value={selectedDate}
-                  onChange={handleDateChange}
+                  autoComplete='given-name'
                 />
               </Grid>
-              <Grid item xs={1} sm={4}>
-                <KeyboardDatePicker
-                  disableToolbar
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  id='numFacture'
+                  name='numFacture'
+                  label='Numéro de Facture'
                   variant='outlined'
-                  format='MM/dd/yyyy'
-                  margin='normal'
-                  id='date-echance'
-                  label="Date d'échéance"
-                  value={selectedDate}
-                  onChange={handleDateChange}
+                  size='small'
+                  fullWidth
+                  autoComplete='family-name'
                 />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  required
+                  id='devise'
+                  name='devise'
+                  label='Devise'
+                  variant='outlined'
+                  size='small'
+                  fullWidth
+                  autoComplete='family-name'
+                />
+              </Grid>
+              <Grid
+                container
+                spacing={0}
+                direction='row'
+                justifyContent='center'
+                color='#252831'
+              >
+                <Grid item xs={1} sm={4}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant='outlined'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='date-creation'
+                    label='Date de la création'
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                  />
+                </Grid>
+                <Grid item xs={1} sm={4}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant='outlined'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='date-echance'
+                    label="Date d'échéance"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <TableFacture />
-        </Paper>
-      </MuiPickersUtilsProvider>
-    </main>
+            <TableFacture />
+          </Paper>
+        </MuiPickersUtilsProvider>
+      </main>
+    </Fragment>
   )
 }
